423ef5f884f8f81742d267c6e850f769b8aa09b6
ilyessbt <ilyessbtekaya04@gmail.com>
2023-01-31 14:44:14 +0100

    (error)

--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderDto } from '../dtos/order.dto';
+import { OrderEntity } from '../models/order.entity';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(
+        @InjectRepository(OrderEntity)
+        private readonly OrderRepository: Repository<OrderEntity>
+    ) { }
+
+
+    create(order: OrderDto): Promise<OrderEntity> {
+        return this.OrderRepository.save(order);
+    }
+}
